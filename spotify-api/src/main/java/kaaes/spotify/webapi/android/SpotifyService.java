5a49ee1d7427be70300d1ccd29fd98fe644b4671
koningWillemAlexander <s.a.houtenbos@student.tue.nl>
2019-04-04 13:11:02 +0200

    ECHT HANDIG DIT MAN

--- spotify-api/src/main/java/kaaes/spotify/webapi/android/SpotifyService.java
+++ spotify-api/src/main/java/kaaes/spotify/webapi/android/SpotifyService.java
@@ -1834,7 +1834,7 @@ public interface SpotifyService {
     Result pause(@Query("device_id") String ids);
 
     @PUT("/me/player/play")
-    void play(@Query("device_id") String ids, @Query("context_uri") String context_uri);
+    void play(@Query("device_id") String ids, @Query("context_uri") String context_uri, Callback<Result> callback);
 
     @POST("/me/player/next")
     Result next(@Query("device_id") String ids);
